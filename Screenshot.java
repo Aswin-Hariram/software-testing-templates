7751d523f0efb9f3cb8deca4dbaeeef602d69d1c
Aswin Hariram <aswincseskct@gmail.com>
2025-09-17 20:58:57 +0530

    Update Screenshot.java

--- Screenshot.java
+++ Screenshot.java
@@ -12,26 +12,28 @@ import com.google.common.io.Files;
 
 public class Screenshot extends Base {
 
-	public static TakesScreenshot ts;
+    public static TakesScreenshot ts;
 
-	public static void captureScreenShot(String filename) {
-		String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-		String name = filename + timestamp + ".png";
-		ts = (TakesScreenshot) driver;
-		File file = ts.getScreenshotAs(OutputType.FILE);
+    public static String captureScreenShot(String filename) {
+        String timestamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String name = filename + "_" + timestamp + ".png";
+        ts = (TakesScreenshot) driver;
+        File file = ts.getScreenshotAs(OutputType.FILE);
 
-		// Create the screenshots directory if it doesn't exist
-		File screenshotsDir = new File(System.getProperty("user.dir") + "/screenshots");
-		if (!screenshotsDir.exists()) {
-			screenshotsDir.mkdirs();
-		}
+        // Create the screenshots directory if it doesn't exist
+        File screenshotsDir = new File(System.getProperty("user.dir") + "/screenshots");
+        if (!screenshotsDir.exists()) {
+            screenshotsDir.mkdirs();
+        }
 
-		File target = new File(screenshotsDir, name);
-		try {
-			Files.copy(file, target);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        File target = new File(screenshotsDir, name);
+        try {
+            Files.copy(file, target);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        // Return the full path of the screenshot
+        return target.getAbsolutePath();
+    }
 }
